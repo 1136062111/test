ccac2cd25d06bec25784e1c0a725db91fd317598
11360 <11360@DESKTOP-91Q5BPK>
2018-06-15 03:15:08 +0900

    init

--- src/cmq/lkjx/cn/webservice/main/Test.java
+++ src/cmq/lkjx/cn/webservice/main/Test.java
@@ -1,10 +1,18 @@
 package cmq.lkjx.cn.webservice.main;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Map;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("");
+		Hashtable<String,String> hashtable= new Hashtable<>();
+		HashSet<String> hashset= new HashSet<>();
+		Map<String, String> map = new HashMap<>();
 	}
 
 }
